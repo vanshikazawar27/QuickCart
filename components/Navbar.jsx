08e539a0ad18b8cd26c19efab1a0e659a14bee40
unknown <vanshikazawar@gmail.com>
2025-11-18 13:19:22 +0530

    mongodb connected

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,6 @@
 "use client"
 import React from "react";
-import { assets, CartIcon } from "@/assets/assets";
-import React from "react";
-import { assets, BagIcon } from "@/assets/assets";
+import { assets, CartIcon, BagIcon, HomeIcon, BoxIcon } from "@/assets/assets";
 import Link from "next/link"
 import { useAppContext } from "@/context/AppContext";
 import Image from "next/image";
@@ -61,10 +59,29 @@ const Navbar = () => {
 
       <div className="flex items-center md:hidden gap-3">
         {isSeller && <button onClick={() => router.push('/seller')} className="text-xs border px-4 py-1.5 rounded-full">Seller Dashboard</button>}
-        <button onClick={openSignIn} className="flex items-center gap-2 hover:text-gray-900 transition">
+        {
+          user
+            ? <>
+            <UserButton>
+              <UserButton.MenuItems>
+                <UserButton.Action label="Home" labelIcon={<HomeIcon />} onClick={() => router.push('/')} />
+              </UserButton.MenuItems>
+              <UserButton.MenuItems>
+                <UserButton.Action label="Products" labelIcon={<BoxIcon />} onClick={() => router.push('/all-products')} />
+              </UserButton.MenuItems>
+              <UserButton.MenuItems>
+                <UserButton.Action label="Cart" labelIcon={<CartIcon />} onClick={() => router.push('/cart')} />
+              </UserButton.MenuItems>
+              <UserButton.MenuItems>
+                <UserButton.Action label="My Orders" labelIcon={<BagIcon />} onClick={() => router.push('/my-orders')} />
+              </UserButton.MenuItems>
+            </UserButton>
+            </> 
+            : <button onClick={openSignIn} className="flex items-center gap-2 hover:text-gray-900 transition">
           <Image src={assets.user_icon} alt="user icon" />
           Account
         </button>
+        }
       </div>
     </nav>
   );
