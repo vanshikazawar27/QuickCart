f89c623df412ddfdeba24a203ca279d12cc5f144
Vanshika Zawar <vanshikazawar@gmail.com>
2026-01-06 16:52:21 +0530

    update inngest fucntion

--- config/inngest.js
+++ config/inngest.js
@@ -7,22 +7,27 @@ export const inngest = new Inngest({ id: "quickcart-next" });
 
 //inngest function  to save user data to a database
 export const syncUserCreation = inngest.createFunction(
-    {
-        id:'sync-user-from-clerk'
-    },
-    {event: 'clerk/user.created'},
-  async ({event}) => {
-      const {id, first_name, last_name, email_addresses, image_url } = event.data
-      const userData= {
-        _id:id,
-        email: email_addresses[0].email_address,
-        name: first_name + ' ' + last_name,
-        imageUrl: image_url
-      }
-      await connectDB()
-      await User.create(userData)
+  { id: "sync-user-from-clerk" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
+
+    await connectDB();
+
+    await User.updateOne(
+      { _id: id },
+      {
+        $set: {
+          email: email_addresses[0].email_address,
+          name: `${first_name} ${last_name}`,
+          imageUrl: image_url,
+        },
+      },
+      { upsert: true }
+    );
   }
-)
+);
+
 
 //inngest function to update user data in database
 export const syncUserUpdation = inngest.createFunction(
