dfdf90dc70b78b5ab8290bf446141a042471af7b
Vanshika Zawar <vanshikazawar@gmail.com>
2026-01-06 16:34:24 +0530

    fix type issue in user model in inggest at image url

--- config/inngest.js
+++ config/inngest.js
@@ -17,7 +17,7 @@ export const syncUserCreation = inngest.createFunction(
         _id:id,
         email: email_addresses[0].email_address,
         name: first_name + ' ' + last_name,
-        imageURL: image_url
+        imageUrl: image_url
       }
       await connectDB()
       await User.create(userData)
