6ee4b493cc36049b3c307d4b38f269d3efae9e1f
Vanshika Zawar <vanshikazawar@gmail.com>
2026-01-06 16:18:22 +0530

    changes in db and change the database creation way

--- config/db.js
+++ config/db.js
@@ -1,24 +1,58 @@
+// import mongoose from "mongoose";
+
+// let cached = global.mongoose;
+
+// if (!cached) {
+//     cached = global.mongoose = { conn: null, promise: null };
+// }
+
+// async function connectDB() {
+//     if (cached.conn) return cached.conn;
+
+//     if (!cached.promise) {
+//         const opts = {
+//             bufferCommands: false,
+//         };
+
+//         cached.promise = mongoose
+//             .connect(`${process.env.MONGODB_URI}/quickcart`, opts)
+//             .then((mongoose) => {
+//                 console.log("✅ MongoDB connected");
+//                 return mongoose;
+//             });
+//     }
+
+//     cached.conn = await cached.promise;
+//     return cached.conn;
+// }
+
+// export default connectDB;
+
+
 import mongoose from "mongoose";
 
 let cached = global.mongoose;
 
 if (!cached) {
-    cached = global.mongoose = { conn: null, promise: null };
+  cached = global.mongoose = { conn: null, promise: null };
 }
+
 async function connectDB() {
-    if (cached.conn) {
-        return cached.conn;
-    }
-    if (!cached.promise) {
-        const opts = {
-            bufferCommands: false
-        };
-        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/quickcart`, opts).then((m) => {
-            return m;
-        });
-    }
-    cached.conn = await cached.promise;
-    return cached.conn;
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(process.env.MONGODB_URI, {
+        bufferCommands: false,
+      })
+      .then((mongoose) => {
+        console.log("✅ MongoDB connected");
+        return mongoose;
+      });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
 export default connectDB;
