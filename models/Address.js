8da313f45077741ffbe845b2179e93434c7d7bc6
Vanshika Zawar <vanshikazawar@gmail.com>
2026-02-02 07:17:54 +0530

    `Update Image src and alt attributes in Cart component`

---
+++ models/Address.js
@@ -0,0 +1,16 @@
+import { PhoneNumber } from "@clerk/nextjs/dist/types/server";
+import mongoose from "mongoose";
+
+const addressSchema = new mongoose.Schema({
+    userId: {type: String, required: true},
+    fullName: {type: String, required: true},
+    PhoneNumber: {type: String, required: true},
+    pincode: {type: String, required: true},
+    area: {type: String, required: true},
+    city: {type: String, required: true},
+    state: {type: String, required: true},
+})
+
+const Address = mongoose.models.address || mongoose.model('address', addressSchema)
+
+export default Address
